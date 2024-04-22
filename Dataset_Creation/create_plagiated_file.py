dacfabfa393e6113834fe5d38a96dec330b8a6d6
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-22 17:26:47 +0200

    applied on the file

--- Dataset_Creation/create_plagiated_file.py
+++ Dataset_Creation/create_plagiated_file.py
@@ -4,15 +4,30 @@ from transformers import BartForConditionalGeneration, BartTokenizer
 model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
 
-# Original text
-original_text = """Apart from counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism. To check word count, simply place your cursor into the text box above and start typing. You'll see the number of characters and words increase or decrease as you type, delete, and edit them. You can also copy and paste text from another program over into the online editor above.
-"""
+# Load text file
+file_path = "output_test\cleaned_text.txt"
+with open(file_path, "r", encoding="utf-8") as file:
+    text = file.read()
+
+# Find the starting index of the abstract
+abstract_start_index = text.find("Abstract")
+
+# Find the ending index of the abstract
+abstract_end_index = text.find("1 Introduction")
+
+# Extract the abstract section
+abstract = text[abstract_start_index:abstract_end_index].strip()
+
+# Print abstract
+print("Abstract:")
+print(abstract)
+print()
 
 # Prompt for paraphrasing
-prompt = "paraphrase: "
+prompt = "paraphrase it: \n"
 
-# Combine prompt and original text
-sequence = prompt + original_text
+# Combine prompt and abstract
+sequence = prompt + abstract
 
 # Tokenize input text
 inputs = tokenizer(sequence, return_tensors="pt", max_length=1024, truncation=True)
@@ -21,7 +36,8 @@ inputs = tokenizer(sequence, return_tensors="pt", max_length=1024, truncation=Tr
 paraphrased_ids = model.generate(**inputs)
 
 # Decode paraphrased text
-paraphrased_text = tokenizer.decode(paraphrased_ids[0], skip_special_tokens=True)
+paraphrased_abstract = tokenizer.decode(paraphrased_ids[0], skip_special_tokens=True)
 
-# Print paraphrased text
-print(paraphrased_text)
+# Print paraphrased abstract
+print("\nParaphrased Abstract:")
+print(paraphrased_abstract)
