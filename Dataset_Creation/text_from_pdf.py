7a4a04ea5f01fcffae231f484fde4792aba2f660
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-23 12:18:13 +0200

    Adjust text extraction

--- Dataset_Creation/text_from_pdf.py
+++ Dataset_Creation/text_from_pdf.py
@@ -1,18 +1,19 @@
-import pdfplumber
+from pypdf import PdfReader
 
 def extract_text_from_pdf(pdf_path):
-    text = ""
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text()
-    return text
+  text=''
+  reader = PdfReader(pdf_path) 
+  print(len(reader.pages)) 
+  for i in range(len(reader.pages)):
+    text = text + reader.pages[i].extract_text() 
+  return text
 
 def save_text_to_file(text, output_file):
     with open(output_file, "w", encoding="utf-8") as file:
         file.write(text)
 
 # Example usage
-pdf_path = r"Dataset_Collection\original_paper\0704.0004v1.A_determinant_of_Stirling_cycle_numbers_counts_unlabeled_acyclic_single_source_automata.pdf"
+pdf_path = r"Adaptive RL with LLM-augmented reward functions.pdf"
 output_file = "output_test/extracted_text.txt"
 
 pdf_text = extract_text_from_pdf(pdf_path)
