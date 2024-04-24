9b7e5133dbb15a399678106e816d23729d59e1d9
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-24 14:37:07 +0200

    To test different model for the generation part

--- Dataset_Creation/create_plagiated_file.py
+++ Dataset_Creation/create_plagiated_file.py
@@ -4,17 +4,16 @@ import plagiated_function
 input_file_path = 'output_test\cleaned_text.txt'
 output_file_path = 'output_test\cleaned_text_with_paraphrased_abstract.txt'
 
-"""
+'''
 # Paraphrase the abstract in the input file and write the modified content to a new file
 plagiated_function.paraphrase_abstract_in_file(input_file_path, output_file_path)
 
 print("The Abstract was correct modified and saved:", output_file_path)
+'''
 
 
-"""
 
 '''
-from transformers import pipeline
 
 from transformers import pipeline
 
@@ -25,16 +24,15 @@ def get_title(file_path):
     return title
 
 def generate_introduction(title):
-    generator = pipeline("text-generation", model="gpt2-medium", tokenizer="gpt2-medium")
-    input_text = f"Generate an introduction based on the title: '{title}'.\n\nIntroduction:"
+# TEST THE MODEL ON THE GENERATION PART, COMPARE WITH T5-BASE
+    generator = pipeline("text-generation", model="gpt2-medium", tokenizer="gpt2-medium") 
+    input_text = f"Generate an introduction based on the title: '{title}'.\n\nIntroduction:"  #test to get the best prompt
     generated_text = generator(input_text, max_length=200, temperature=0.7, num_return_sequences=1, do_sample=True, early_stopping=True)[0]['generated_text']
     return generated_text
 
-# Example usage:
 title = get_title(input_file_path)
 intro = generate_introduction(title)
 print(intro)
 '''
-
 plagiated_function.change_introduction(input_file_path)
 
