459d728629ea54db74ea1a37184e2a5f45983ca7
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-25 12:28:08 +0200

    working on xml file

--- Dataset_Creation_temp/paper_extraction.py
+++ Dataset_Creation_temp/paper_extraction.py
@@ -1,15 +1,30 @@
 import xml.etree.ElementTree as ET
 
-file_path = "your_file.xml"  # Replace "your_file.xml" with the actual file path
+# File path
+file_path = "PMC13900.xml"  # Replace "your_file.xml" with the actual file path
 
 # Parse XML from file
 tree = ET.parse(file_path)
 root = tree.getroot()
 
-# Find the passage with section_type 'ABSTRACT'
-abstract_passage = root.find(".//passage[infon[@key='section_type' and text()='ABSTRACT']]")
+# Find the abstract passage
+'''
+abstract_passage = None
+for passage in root.findall(".//passage"):
+    section_type = passage.find("infon[@key='section_type']")
+    if section_type is not None and section_type.text == 'ABSTRACT':
+        abstract_passage = passage
+        break
 
-# Extract abstract text
-abstract_text = abstract_passage.find("text").text
+if abstract_passage is not None:
+    # Extract abstract text
+    abstract_text = abstract_passage.find("text").text
+    print(abstract_text)
+else:
+    print("Abstract passage not found.")
+'''
 
-print(abstract_text)
+passage = root.findall('.//passage')
+for infon in passage:
+    text = infon.find(".//infon[@key='section_type']")
+    print(text.text)
