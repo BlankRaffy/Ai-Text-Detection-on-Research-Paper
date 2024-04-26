42046392be5f03105efa8789b98caebf38367d41
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-26 12:54:17 +0200

    implemented function to create abstract based on xml and made some code cleaning

---
+++ Dataset_Creation_temp/create_plagiated_paper.py
@@ -0,0 +1,6 @@
+import plagiated_function
+import os
+
+input_path= 'origianal_paper\PMC13901.xml'
+tree = plagiated_function.change_abstract(input_path)
+#plagiated_function.save_tree(input_path,tree)
