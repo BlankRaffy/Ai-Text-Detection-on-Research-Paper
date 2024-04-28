9fdbf0387038511f11457e51da1b1ceeaa8dbde3
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-28 12:14:24 +0200

    generate all part for the plagated introduction ,we have to improve the generation quality

--- Dataset_Creation/plagiated_function.py
+++ Dataset_Creation/plagiated_function.py
@@ -11,28 +11,34 @@ tokenizer = AutoTokenizer.from_pretrained("humarin/chatgpt_paraphraser_on_T5_bas
 model = AutoModelForSeq2SeqLM.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base").to(device)
 
 def paraphrase(
-    question,
+    sentence,
     num_beams=5,
     num_beam_groups=5,
     num_return_sequences=1,
-    repetition_penalty=10.0,
-    diversity_penalty=3.0,
+    repetition_penalty=5.0,
+    diversity_penalty=10.0,
     no_repeat_ngram_size=2,
     temperature=0.7,
-    max_length=512
+    max_length=400  # Increase max_length to accommodate longer outputs
 ):
     input_ids = tokenizer(
-        f'paraphrase: {question}',
-        return_tensors="pt", padding="longest",
+        f'rephrase: {sentence}',
+        return_tensors="pt",
+        padding="longest",
         max_length=max_length,
         truncation=True,
     ).input_ids.to(device)
     
     outputs = model.generate(
-        input_ids, temperature=temperature, repetition_penalty=repetition_penalty,
-        num_return_sequences=num_return_sequences, no_repeat_ngram_size=no_repeat_ngram_size,
-        num_beams=num_beams, num_beam_groups=num_beam_groups,
-        max_length=max_length, diversity_penalty=diversity_penalty
+        input_ids,
+        temperature=temperature,
+        repetition_penalty=repetition_penalty,
+        num_return_sequences=num_return_sequences,
+        no_repeat_ngram_size=no_repeat_ngram_size,
+        num_beams=num_beams,
+        num_beam_groups=num_beam_groups,
+        max_length=max_length,
+        diversity_penalty=diversity_penalty
     )
 
     res = tokenizer.batch_decode(outputs, skip_special_tokens=True)
@@ -45,7 +51,9 @@ def generate_introduction(title):
     generator = pipeline("text-generation", model="gpt2-medium", tokenizer="gpt2-medium") 
     input_text = f"Generate an introduction for a medical reseach titeled: '{title}'.\n\nIntroduction:"  #test to get the best prompt
     generated_text = generator(input_text, max_length=200, temperature=0.6, num_return_sequences=1, do_sample=True, early_stopping=True)[0]['generated_text']
-    return generated_text
+    intro_text = generated_text.split('\n\nIntroduction:')[-1].strip()
+    return intro_text
+
 
 def change_abstract(tree):
     
@@ -75,7 +83,7 @@ def change_abstract(tree):
 
 def change_intro(tree):
     root = tree.getroot()
-    
+    generated = True #boolean to to modify the first element of the introduction
     # Trova tutti gli elementi 'passage' nell'albero XML 
     passages = root.findall('.//passage')
     
@@ -92,7 +100,22 @@ def change_intro(tree):
         
         if section_type is not None and section_type.text == 'INTRO':
             if passage.find('.//text').text != 'Introduction':
-                print(passage.find('.//text').text+'\n\n')
+                #Adding the generated introduction based on the title
+                if generated:
+                    passage.find('.//text').text = introduction_first
+                    print(introduction_first)
+                    generated = False
+                #rephrase the other part of the introduction
+                else:
+                    text_to_rephrase = passage.find('.//text').text
+                    print(f"TESTO DA RIFRASERE :\n\n {text_to_rephrase}")
+                    print(f"TESTO RIFRASATO: \n\n {paraphrase(text_to_rephrase)}")
+                    
+                
+            
+
+    return tree
+
 
 
 def save_tree(file_path,tree):
