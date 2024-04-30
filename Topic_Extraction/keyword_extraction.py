a222c144443038b5fb7b579246db3a23e3f00fa6
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-04-30 11:51:56 +0200

    created some function to extraxt text from the xml file and implemented a keyword extraction

---
+++ Topic_Extraction/keyword_extraction.py
@@ -0,0 +1,23 @@
+import get_text
+import xml.etree.ElementTree as ET
+
+
+# Example texts
+import yake
+kw_extractor = yake.KeywordExtractor()
+language = "en"
+max_ngram_size = 3
+deduplication_threshold = 0.9
+numOfKeywords = 6
+custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+
+path=r'C:\Users\Blank\Desktop\Ai-Text-Detection-on-Research-Paper\Topic_Extraction\PMC29044_plagiated.xml'
+tree = ET.parse(path)
+text = get_text.extract_conclusion(tree)
+print(text)
+
+keywords = custom_kw_extractor.extract_keywords(text)
+
+print("\n\n")
+for kw in keywords:
+    print(kw[0])
