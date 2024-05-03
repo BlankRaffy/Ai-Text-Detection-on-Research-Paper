c06819905c722555ff87607b419827b834b4de92
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-05-03 16:21:45 +0200

    working on the compute topic similarity

--- Similarity_Compare/compute_similarity.py
+++ Similarity_Compare/compute_similarity.py
@@ -16,26 +16,38 @@ def convert_to_list(string):
 df_original = pd.read_csv('Topic_Extraction/topic/original_topic.csv', converters={'abstract': convert_to_list, 'introduction': convert_to_list, 'conclusion': convert_to_list})
 df_plagiated= pd.read_csv('Topic_Extraction/topic/plagiated_topic.csv', converters={'abstract': convert_to_list, 'introduction': convert_to_list, 'conclusion': convert_to_list})
 
-origianal_abstact_tp = df_original['abstract'][0]
-print(origianal_abstact_tp[0])
+original_abstact_tp = df_original['abstract'][1]
+plagiated_abstract_tp = df_plagiated['abstract'][1]
+print(original_abstact_tp)
+print(plagiated_abstract_tp)
 
-'''
-originale_tp_abstract = ['breast cancer susceptibility',  'cancer susceptibility genes',  'susceptibility genes encode',  'normal cellular functions', 'genes encode proteins', 'breast cancer']
-plagiated_tp_abstract = ['complex cellular functions',  'encode breast cancer',  'breast cancer susceptibility',  'multiple complex cellular',  'cancer susceptibility genes', 'cellular functions']
+original_intro_tp = df_original['introduction'][1]
+plagiated_intro_tp = df_plagiated['introduction'][1]
+print(original_intro_tp)
+print(plagiated_intro_tp)
+
+original_conclusion_tp = df_original['conclusion'][1]
+plagiated_conclusion_tp = df_plagiated['conclusion'][1]
+print(original_conclusion_tp)
+print(plagiated_conclusion_tp)
+
 
 abstract_similarity = []
+intro_similarity = []
+conclusion_similarity = []
 
-for ele in originale_tp_abstract:
+
+for kw in plagiated_abstract_tp:
     max_similarity = 0
-    word1 = nlp(ele)
-    for item in plagiated_tp_abstract:
-        word2 = nlp(item)
-        similarity_score = word1.similarity(word2)
+    kw1 = nlp(kw)
+    for kw in original_abstact_tp:
+        kw2 = nlp(kw)
+        similarity_score = kw1.similarity(kw2)
         #print(f' la similarità semantica tra {word1} e {word2} è: {similarity_score}')
         if similarity_score > max_similarity:
             max_similarity=similarity_score
     abstract_similarity.append(max_similarity)
 
 print('il massimo di similarità di ogni parola è:', sum(abstract_similarity)/len(abstract_similarity))
-'''
+
 
