84d47edaf6d5d3840dd314a67b1e769b7062c4b5
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-05-04 17:02:59 +0200

    Implemeted topic extraction with lda on all file and tested nfm matching

--- Topic_Extraction/get_topic.py
+++ Topic_Extraction/get_topic.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 import tqdm
 
+#varie versioni per creare i vari file usando i vari metodi di estrazione
 '''
 # Definisci le colonne del DataFrame
 columns = ['file', 'abstract', 'introduction', 'conclusion']
@@ -37,6 +38,7 @@ df_yake.to_csv(csv_file_path, index=False)
 print(f"DataFrame salvato con successo in: {csv_file_path}")
 '''
 
+'''
 # Definisci le colonne del DataFrame
 columns = ['file', 'abstract', 'introduction', 'conclusion']
 df_nmf = pd.DataFrame(columns=columns)
@@ -66,3 +68,34 @@ csv_file_path = 'Topic_Extraction/topic/plagiated_topic_nmf.csv'
 df_nmf.to_csv(csv_file_path, index=False)
 
 print(f"DataFrame salvato con successo in: {csv_file_path}")
+'''
+# Definisci le colonne del DataFrame
+columns = ['file', 'abstract', 'introduction', 'conclusion']
+df_lda = pd.DataFrame(columns=columns)
+
+original_paper = 'Dataset/plagiated_paper'
+
+# Estrai tutte le parole chiave da ciascun file
+for file in tqdm.tqdm(os.listdir(original_paper)):
+    path = os.path.join(original_paper, file)
+    abstract, intro, conclusion = topic_extraction.LDA_extraction(path)
+    
+    # Trasforma le liste di parole chiave in stringhe separate da virgole
+    abstract_str = ', '.join(abstract)
+    intro_str = ', '.join(intro)
+    conclusion_str = ', '.join(conclusion)
+    
+    # Creazione di una nuova riga per il DataFrame
+    new_row = {'file': file, 'abstract': abstract_str, 'introduction': intro_str, 'conclusion': conclusion_str}
+    
+    # Aggiungi la nuova riga al DataFrame
+    df_lda = pd.concat([df_lda, pd.DataFrame([new_row])], ignore_index=True)
+
+# Percorso per il file CSV di output
+csv_file_path = 'Topic_Extraction/topic/plagiated_topic_lda.csv'
+
+# Salva il DataFrame nel file CSV
+df_lda.to_csv(csv_file_path, index=False)
+
+print(f"DataFrame salvato con successo in: {csv_file_path}")
+
--- Topic_Extraction/topic_extraction.py
+++ Topic_Extraction/topic_extraction.py
@@ -5,6 +5,23 @@ import yake
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import NMF
 
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from gensim.corpora import Dictionary
+from gensim.models.ldamodel import LdaModel
+import get_text
+import xml.etree.ElementTree as ET
+import re
+
+
+
+# Assicurati di aver scaricato i dati necessari per NLTK
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
 def yake_extraction(file_path):
 
     # Configurazione dell'estrattore di parole chiave YAKE
@@ -90,3 +107,61 @@ def NMF_extraction(file_path):
     conclusion = top_words_conclusion
     
     return abstract,intro,conclusion
+
+
+def LDA_extraction(file_path):
+    def preprocess_text(text):
+    # Tokenizzazione del testo
+        tokens = word_tokenize(text.lower())
+    
+    # Rimozione delle stopwords e dei caratteri non alfabetici
+        tokens = [token for token in tokens if token.isalpha() and token not in stopwords.words('english')]
+    
+    # Lemmatizzazione delle parole
+        lemmatizer = WordNetLemmatizer()
+        tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+        return tokens
+
+    def extract_topics_from_text(text, num_topics):
+    # Preprocessamento del testo
+        preprocessed_text = preprocess_text(text)
+    
+    # Creazione del dizionario e del corpus per un solo documento
+        dictionary = Dictionary([preprocessed_text])
+        corpus = [dictionary.doc2bow(preprocessed_text)]
+    
+    # Creazione e addestramento del modello LDA
+        lda_model = LdaModel(corpus, num_topics=num_topics, id2word=dictionary, passes=15)
+    
+    # Estrazione e stampa dei temi
+        topics = lda_model.print_topics()
+        return topics[0][1] # we only get the first set of topic
+
+    def extract_keywords(topic_distribution):
+    # Utilizza espressioni regolari per trovare le parole tra virgolette e aggiungerle alla lista
+        keywords = re.findall(r'"(.*?)"', topic_distribution)
+        return keywords
+
+# Testiamo la funzione con un singolo testo di input
+    tree = ET.parse(file_path)
+    abstarct =[]
+    intro = []
+    conclusion = []
+
+
+    abstract_text = get_text.extract_conclusion(tree)
+    raw_topic = extract_topics_from_text(abstract_text, num_topics=1)
+    abstarct.append(extract_keywords(raw_topic))
+
+
+    intro_text = get_text.extract_intro(tree)
+    raw_topic = extract_topics_from_text(intro_text, num_topics=1)
+    intro.append(extract_keywords(raw_topic))
+
+
+    conclusion_text = get_text.extract_intro(tree)
+    raw_topic = extract_topics_from_text(conclusion_text, num_topics=1)
+    conclusion.append(extract_keywords(raw_topic))
+
+    return abstarct[0][:6], intro[0][:6], conclusion[0][:6]
