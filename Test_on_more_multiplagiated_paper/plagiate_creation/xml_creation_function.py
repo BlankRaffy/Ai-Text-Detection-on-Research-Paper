d5fb69b778f8d51e58a2e149fe16edbc8c376f5e
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-05-26 18:34:37 +0200

    renamed some file

--- Test_on_more_multiplagiated_paper/plagiate_creation/xml_creation_function.py
+++ Test_on_more_multiplagiated_paper/plagiate_creation/xml_creation_function.py
@@ -2,6 +2,50 @@ import os
 import xml.etree.ElementTree as ET
 import get_text
 from xml.dom import minidom
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM #import neeeded for paraphrase
+from transformers import pipeline
+import copy
+
+device = "cuda"
+
+tokenizer = AutoTokenizer.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base")
+model = AutoModelForSeq2SeqLM.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base").to(device)
+
+def paraphrase(
+    sentence,
+    num_beams=5,
+    num_beam_groups=5,
+    num_return_sequences=1,
+    repetition_penalty=5.0,
+    diversity_penalty=10.0,
+    no_repeat_ngram_size=2,
+    temperature=0.7,
+    max_length=400 
+):
+    input_ids = tokenizer(
+        f'rephrase: {sentence}',
+        return_tensors="pt",
+        padding="longest",
+        max_length=max_length,
+        truncation=True,
+    ).input_ids.to(device)
+    
+    outputs = model.generate(
+        input_ids,
+        temperature=temperature,
+        repetition_penalty=repetition_penalty,
+        num_return_sequences=num_return_sequences,
+        no_repeat_ngram_size=no_repeat_ngram_size,
+        num_beams=num_beams,
+        num_beam_groups=num_beam_groups,
+        max_length=max_length,
+        diversity_penalty=diversity_penalty
+    )
+
+    res = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    return res[0]
+
 
 
 def find_passage_by_section_type(root, section_type):
@@ -12,7 +56,7 @@ def find_passage_by_section_type(root, section_type):
             return passage
     return None
 
-def save_tree(intro_file_path, abstract_file_path, conclusion_file_path):
+def save_tree( abstract_file_path,intro_file_path ,conclusion_file_path,output_file_path):
     # Load intro XML
     abstract_tree = ET.parse(abstract_file_path)
     intro_tree = ET.parse(intro_file_path)
@@ -37,21 +81,24 @@ def save_tree(intro_file_path, abstract_file_path, conclusion_file_path):
     abstract_infon_element = ET.SubElement(abstract_passage_element, "infon", key="section_type")
     abstract_infon_element.text = "ABSTRACT"
     abstract_text_element = ET.SubElement(abstract_passage_element, "text")
-    abstract_text_element.text = abstract_text
+    paraphrase_abstract= paraphrase(abstract_text)
+    abstract_text_element.text = paraphrase_abstract
 
     # INTRO
     intro_passage_element = ET.SubElement(new_root, "passage")
     intro_infon_element = ET.SubElement(intro_passage_element, "infon", key="section_type")
     intro_infon_element.text = "INTRO"
     intro_text_element = ET.SubElement(intro_passage_element, "text")
-    intro_text_element.text = intro_text
+    paraphrase_intro=paraphrase(intro_text)
+    intro_text_element.text = paraphrase_intro
 
     # CONCLUSION
     conclusion_passage_element = ET.SubElement(new_root, "passage")
     conclusion_infon_element = ET.SubElement(conclusion_passage_element, "infon", key="section_type")
     conclusion_infon_element.text = "CONCL"
     conclusion_text_element = ET.SubElement(conclusion_passage_element, "text")
-    conclusion_text_element.text = conclusion_text
+    paraphrase_conclusion= paraphrase(conclusion_text)
+    conclusion_text_element.text = paraphrase_conclusion
 
     # Convert tree to a string
     rough_string = ET.tostring(new_root, 'utf-8')
@@ -59,15 +106,18 @@ def save_tree(intro_file_path, abstract_file_path, conclusion_file_path):
     output_string = reparsed.toprettyxml(indent="  ", encoding='utf-8').decode('utf-8')
 
     # Save the new XML tree to a file
-    output_file_path = "combined_sections.xml"
+    output_file_path = output_file_path
     with open(output_file_path, "wb") as f:
         f.write(output_string.encode('utf-8'))
 
     return output_file_path
 
+'''
 # Example usage:
 intro_file_path = "Dataset/original_paper/PMC13901.xml"
 abstract_file_path = "Dataset/original_paper/PMC29080.xml"
 conclusion_file_path = "Dataset/original_paper/PMC32247.xml"
 combined_file_path = save_tree(intro_file_path, abstract_file_path, conclusion_file_path)
 print("Combined sections saved to:", combined_file_path)
+'''
+#To Compute 
