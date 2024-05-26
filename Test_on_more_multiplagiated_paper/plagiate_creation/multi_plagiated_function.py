e9a9f557ad2a37ec71b2ef9125ffe9e84db65139
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-05-26 12:53:57 +0200

    working on multi_plagiated version

---
+++ Test_on_more_multiplagiated_paper/plagiate_creation/multi_plagiated_function.py
@@ -0,0 +1,125 @@
+import xml.etree.ElementTree as ET
+import os
+
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM #import neeeded for paraphrase
+from transformers import pipeline
+import copy
+
+device = "cuda"
+
+tokenizer = AutoTokenizer.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base")
+model = AutoModelForSeq2SeqLM.from_pretrained("humarin/chatgpt_paraphraser_on_T5_base").to(device)
+
+def paraphrase(
+    sentence,
+    num_beams=5,
+    num_beam_groups=5,
+    num_return_sequences=1,
+    repetition_penalty=5.0,
+    diversity_penalty=10.0,
+    no_repeat_ngram_size=2,
+    temperature=0.7,
+    max_length=400 
+):
+    input_ids = tokenizer(
+        f'rephrase: {sentence}',
+        return_tensors="pt",
+        padding="longest",
+        max_length=max_length,
+        truncation=True,
+    ).input_ids.to(device)
+    
+    outputs = model.generate(
+        input_ids,
+        temperature=temperature,
+        repetition_penalty=repetition_penalty,
+        num_return_sequences=num_return_sequences,
+        no_repeat_ngram_size=no_repeat_ngram_size,
+        num_beams=num_beams,
+        num_beam_groups=num_beam_groups,
+        max_length=max_length,
+        diversity_penalty=diversity_penalty
+    )
+
+    res = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+    return res[0]
+
+
+
+
+
+def change_abstract(tree):
+    
+    root = tree.getroot()
+    
+    # Trova tutti gli elementi 'passage' nell'albero XML 
+    passages = root.findall('.//passage')
+    
+    # Itera su ciascun elemento 'passage'
+    for passage in passages:
+        # Trova l'elemento 'infon' con attributo 'section_type' uguale a 'ABSTRACT'
+        section_type = passage.find(".//infon[@key='section_type']")
+        
+        if section_type is not None and section_type.text == 'ABSTRACT':
+            # Trova l'elemento 'text' all'interno del 'passage' (abstract)
+            abstract_element = passage.find('.//text')
+            
+            if abstract_element is not None:
+                abstract_text = passage.find('.//text').text
+                passage.find('.//text').text = paraphrase(abstract_text)
+                ##print(passage.find('.//text').text+"\n\n")
+    
+    # Restituisci l'albero XML copiato e modificato
+    return tree
+
+def change_intro(tree):
+    root = tree.getroot()
+    generated = True #boolean to to modify the first element of the introduction
+    # Trova tutti gli elementi 'passage' nell'albero XML 
+    passages = root.findall('.//passage')
+    
+    # Itera su ciascun elemento 'passage'
+    for passage in passages:
+        # Trova l'elemento 'infon' con attributo 'section_type' uguale a 'ABSTRACT'
+        section_type = passage.find(".//infon[@key='section_type']")
+        if section_type is not None and section_type.text == 'INTRO':
+            if passage.find('.//text').text != 'Introduction':
+                text_to_rephrase = passage.find('.//text').text
+
+                    ##print(f"TESTO DA RIFRASERE :\n\n {text_to_rephrase}")
+                    ##print(f"TESTO RIFRASATO: \n\n {paraphrase(text_to_rephrase)}")
+                passage.find('.//text').text = paraphrase(text_to_rephrase)
+                    #print(passage.find('.//text').text +'\n\n')
+                
+            
+
+    return tree
+
+def change_conclusion(tree):
+    root = tree.getroot()
+    generated = True #boolean to to modify the first element of the introduction
+    # Trova tutti gli elementi 'passage' nell'albero XML 
+    passages = root.findall('.//passage')
+    
+    # Itera su ciascun elemento 'passage'
+    for passage in passages:
+        # Trova l'elemento 'infon' con attributo 'section_type' uguale a 'CONCL'
+        section_type = passage.find(".//infon[@key='section_type']")
+        
+        if section_type is not None and section_type.text == 'CONCL':
+            if passage.find('.//text').text != 'Conclusion':
+                text_to_rephrase = passage.find('.//text').text
+                passage.find('.//text').text = paraphrase(text_to_rephrase)
+                #print(passage.find('.//text').text + '\n\n')
+    return tree
+
+
+
+
+def save_tree(file_path,tree):
+    file_name_no_ext= os.path.splitext(os.path.basename(file_path))[0]
+    destination_folder = 'Dataset/plagiated_paper'+'/'
+
+    output_path = destination_folder+file_name_no_ext+'_plagiated.xml'
+    tree.write(output_path)
---
+++ Test_on_more_multiplagiated_paper/plagiate_creation/multi_plagiated_paper_creation.py
@@ -0,0 +1,54 @@
+import multi_plagiated_function
+import os
+import xml.etree.ElementTree as ET
+import tqdm
+import pandas as pd
+
+''' TEST ON A SINGLE FILE
+file_path= 'original_paper\PMC13901.xml'
+
+tree = ET.parse(file_path)
+
+tree = plagiated_function.change_abstract(tree)
+tree = plagiated_function.change_intro(tree)
+tree = plagiated_function.change_conclusion(tree)
+
+#save the final xml file with all the modified part:
+plagiated_function.save_tree(file_path,tree)
+
+folder = 'Dataset/original_paper'
+for ele in tqdm.tqdm(os.listdir(folder)):
+
+    absolute_path = folder+'/'+ele
+    
+    tree = ET.parse(absolute_path)
+
+    tree = plagiated_function.change_abstract(tree)
+    tree = plagiated_function.change_intro(tree)
+    tree = plagiated_function.change_conclusion(tree)
+
+    #save the final xml file with all the modified part:
+    plagiated_function.save_tree(absolute_path,tree)
+'''
+
+df = pd.read_csv('Similarity_Compare/top_k_for_similarity/paper_topic_similarity_rank.csv') #ci stiamo basando su quelli che in precedenza avevano il final score più alto
+
+# Ordina per 'input_file' e 'final_score' in ordine decrescente
+df_sorted = df.sort_values(by=['input_file', 'final_score'], ascending=[True, False])
+
+# Prendi i primi 3 per ogni 'input_file'
+df_top3 = df_sorted.groupby('input_file').head(3)
+
+
+# Accedi ai primi 3 elementi del primo gruppo
+first_input_file = df_top3['input_file'].unique()[0] #primo elemento 
+df_first_input_file = df_top3[df_top3['input_file'] == first_input_file] #top_3 per il primo elemtno
+
+file_list=[]
+
+for i in range(len(df_first_input_file)): #iteriamo sui primi 3 elementi e ci salviamo i nomi dei file
+    file_list.append(df_first_input_file.iloc[i]['compare_file'])
+
+print(file_list)
+
+
