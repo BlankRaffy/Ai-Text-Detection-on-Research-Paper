cb854dd3d9698a51851224909a856be8a2f814bc
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-05-26 13:00:17 +0200

    working of creation of mult plagiated

--- Test_on_more_multiplagiated_paper/plagiate_creation/multi_plagiated_paper_creation.py
+++ Test_on_more_multiplagiated_paper/plagiate_creation/multi_plagiated_paper_creation.py
@@ -33,6 +33,9 @@ for ele in tqdm.tqdm(os.listdir(folder)):
 
 df = pd.read_csv('Similarity_Compare/top_k_for_similarity/paper_topic_similarity_rank.csv') #ci stiamo basando su quelli che in precedenza avevano il final score più alto
 
+columns=['input_file','list_of_source_file']
+df_multi_plagiated_source=pd.DataFrame(columns=columns)
+
 # Ordina per 'input_file' e 'final_score' in ordine decrescente
 df_sorted = df.sort_values(by=['input_file', 'final_score'], ascending=[True, False])
 
@@ -49,6 +52,25 @@ file_list=[]
 for i in range(len(df_first_input_file)): #iteriamo sui primi 3 elementi e ci salviamo i nomi dei file
     file_list.append(df_first_input_file.iloc[i]['compare_file'])
 
-print(file_list)
+new_row = {'input_file':df_first_input_file.iloc[i]['input_file'],'list_of_source_file':file_list}
+
+df_multi_plagiated_source = pd.concat([df_multi_plagiated_source, pd.DataFrame([new_row])], ignore_index=True)
+
+original_dataset='Dataset/original_paper'
+
+absolute_path_abstract = original_dataset+'/'+file_list[0]
+    
+abstract_tree = ET.parse(absolute_path_abstract)
+'''
+tree = multi_plagiated_function.change_abstract(tree)
+tree = plagiated_function.change_intro(tree)
+tree = plagiated_function.change_conclusion(tree)
+
+#save the final xml file with all the modified part:
+plagiated_function.save_tree(absolute_path,tree)
+
+'''
+
+
 
 
