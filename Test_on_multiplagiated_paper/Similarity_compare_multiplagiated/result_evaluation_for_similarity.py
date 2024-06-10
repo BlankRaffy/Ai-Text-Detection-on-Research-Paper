6d6fdc332a635bfad40c35c2780c2fa7579f2888
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-06-10 10:07:18 +0200

    evaluated similarity ranking on top5

---
+++ Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/result_evaluation_for_similarity.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import ast
+
+df_abstract = pd.read_csv('Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/top-k_rank/top_abstract_scores.csv').groupby('input_file')
+df_intro = pd.read_csv('Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/top-k_rank/top_intro_scores.csv').groupby('input_file')
+df_conclusion = pd.read_csv('Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/top-k_rank/top_conclusion_scores.csv').groupby('input_file')
+df_source= pd.read_csv('Test_on_multiplagiated_paper\multiplagiated_source.csv')
+df_source['list_of_source_file'] = df_source['list_of_source_file'].apply(ast.literal_eval)
+
+i=0
+abstrac_result=0
+for ele in df_abstract:
+    for item in ele[1]['compare_file']:
+        if (df_source.iloc[i]['list_of_source_file'][0]) == item: #the first one is the one from we get the abstract
+            abstrac_result +=1
+    i+=1
+
+
+
+i=0
+intro_result=0
+for ele in df_intro:
+    for item in ele[1]['compare_file']:
+        test=df_source.iloc[i]['list_of_source_file'][1]
+        print(f'source file is : {test} the file on the group is {item}' )
+        if (df_source.iloc[i]['list_of_source_file'][1]) == item: #the second one is the one from we get the intro
+            intro_result +=1
+    i+=1
+
+
+i=0
+conclusion_result=0
+for ele in df_conclusion:
+    for item in ele[1]['compare_file']:
+        if (df_source.iloc[i]['list_of_source_file'][2]) == item: #the third one is the one from we get the abstract
+            conclusion_result +=1
+    i+=1
+
+print(f'precision on abstact with top-5 is {abstrac_result/174}')   
+print(f'precision on intro with top-5 is {intro_result/174}')  
+print(f'precision on conclusion with top-5 is {conclusion_result/174}')  
--- Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/similarity_rank_multiplagiated.py
+++ Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/similarity_rank_multiplagiated.py
@@ -30,8 +30,8 @@ top_intros_df = pd.concat(top_intros, ignore_index=True)
 top_conclusions_df = pd.concat(top_conclusions, ignore_index=True)
 
 # Save the top scores to CSV files
-top_abstracts_df.to_csv('top_abstract_scores.csv', index=False)
-top_intros_df.to_csv('top_intro_scores.csv', index=False)
-top_conclusions_df.to_csv('top_conclusion_scores.csv', index=False)
+top_abstracts_df.to_csv('Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/top-k_rank/top_abstract_scores.csv', index=False)
+top_intros_df.to_csv('Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/top-k_rank/top_intro_scores.csv', index=False)
+top_conclusions_df.to_csv('Test_on_multiplagiated_paper/Similarity_compare_multiplagiated/top-k_rank/top_conclusion_scores.csv', index=False)
 
 print("Top scores have been saved to CSV files.")
