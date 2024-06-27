71274488648ef61623916769b4565c0ddcd2b96c
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-06-27 12:42:33 +0200

    change color of the graph

--- Similarity_Compare/graph/graph_for_similarity_compare.py
+++ Similarity_Compare/graph/graph_for_similarity_compare.py
@@ -6,7 +6,7 @@ file_path = 'Similarity_Compare/graph/dataframe_for_graphic.csv'
 data = pd.read_csv(file_path)
 
 # Select the first 10 elements (1st to 10th)
-data = data.iloc[1:15]
+data = data.iloc[1:25] #with 25 we got 7, 20 we got and with 15 we got 12
 
 # Calculate the heights for each section of the bar
 data['abstract_section'] = data['abstract_score'] * 0.4
@@ -14,7 +14,7 @@ data['intro_section'] = data['intro_score'] * 0.2
 data['conclusion_section'] = data['conclusion_score'] * 0.4
 
 # Define colors for each section
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c']  # Blue, Orange, Green
+colors = ['#E76F51', '#F4A261', '#E9C46A']  
 
 # Plotting the stacked bar chart
 fig, ax = plt.subplots(figsize=(10, 6))
