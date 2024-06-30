aac1556890ff1c8df6da17f36afb1195e551584f
BlankRaffy <raffaeleferriero1999@gmail.com>
2024-06-30 11:29:59 +0200

    generated_orizontal graph for file

--- Similarity_Compare/graph/graph_for_similarity_compare.py
+++ Similarity_Compare/graph/graph_for_similarity_compare.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 file_path = 'Similarity_Compare/graph/dataframe_for_graphic.csv'
 data = pd.read_csv(file_path)
 
-# Select the first 10 elements (1st to 10th)
-data = data.iloc[1:20] #with 25 we got 7, 20 we got and with 15 we got 12
+# Select the first 20 elements (2nd to 21st)
+data = data.iloc[0:20]
 
 # Calculate the heights for each section of the bar
 data['abstract_section'] = data['abstract_score'] * 0.4
@@ -14,26 +14,26 @@ data['intro_section'] = data['intro_score'] * 0.2
 data['conclusion_section'] = data['conclusion_score'] * 0.4
 
 # Define colors for each section
-colors = ['#E76F51', '#F4A261', '#E9C46A']  
+colors = ['#E76F51', '#F4A261', '#E9C46A']
 
-# Plotting the stacked bar chart
+# Plotting the stacked horizontal bar chart
 fig, ax = plt.subplots(figsize=(10, 6))
 
-bar_width = 0.5
+bar_height = 0.5
 bars = range(len(data))
 
 # Stacking the bars with specified colors
-ax.bar(bars, data['abstract_section'], bar_width, label='Abstract', color=colors[0])
-ax.bar(bars, data['intro_section'], bar_width, bottom=data['abstract_section'], label='Introduction', color=colors[1])
-ax.bar(bars, data['conclusion_section'], bar_width, 
-       bottom=data['abstract_section'] + data['intro_section'], label='Conclusion', color=colors[2])
+ax.barh(bars, data['abstract_section'], bar_height, label='Abstract', color=colors[0])
+ax.barh(bars, data['intro_section'], bar_height, left=data['abstract_section'], label='Introduction', color=colors[1])
+ax.barh(bars, data['conclusion_section'], bar_height, 
+        left=data['abstract_section'] + data['intro_section'], label='Conclusion', color=colors[2])
 
 # Adding labels and title
-ax.set_xlabel('Files')
-ax.set_ylabel('Scores')
+ax.set_ylabel('Files')
+ax.set_xlabel('Scores')
 ax.set_title('Scores by Section for Each File')
-ax.set_xticks(bars)
-ax.set_xticklabels(data['input_file'], rotation=45, ha='right')
+ax.set_yticks(bars)
+ax.set_yticklabels(data['input_file'])
 ax.legend()
 
 # Show the plot
